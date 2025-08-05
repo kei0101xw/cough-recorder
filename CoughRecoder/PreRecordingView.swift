235dd870e92f8605668f44fc03eec8d0b0393a59
kei0101xw <kei0101xw@icloud.com>
2025-08-05 17:17:09 +0900

    [add] 録音前の注意事項などを表示する画面を作成

---
+++ CoughRecoder/PreRecordingView.swift
@@ -0,0 +1,61 @@
+//
+//  PreRecordingView.swift
+//  CoughRecoder
+//
+//  Created by 原田佳祐 on 2025/08/05.
+//
+
+import SwiftUI
+
+struct PreRecordingView: View {
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        VStack {
+            Text("録音を開始します")
+                .font(.system(size: 60))
+                .padding()
+            
+            Text("""
+                ・静かな環境でお願いします。
+                ・「咳をしてみてください」などの、周辺からの話しかけは録音時はしないでください。
+                ・次のカウントダウン中に「次の画面になったら咳をしましょうね」などと指示してください。
+                """)
+            .font(.system(size: 40))
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
+            
+            Spacer()
+            
+            HStack {
+                            Button(action: {
+                                dismiss()
+                            }) {
+                                Text("戻る")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 60)
+                                    .font(.system(size: 40))
+                                    .padding()
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(10)
+                            }
+
+                            NavigationLink(destination: RecordingView()) {
+                                Text("録音開始")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 60)
+                                    .font(.system(size: 40))
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                        }
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    PreRecordingView()
+}
