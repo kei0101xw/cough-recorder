7d9728c959433e97282e735b607425d315a1d002
kei0101xw <kei0101xw@icloud.com>
2025-08-05 14:56:31 +0900

    [add] 咳記録ログ画面を作成

---
+++ CoughRecoder/CoughLogView.swift
@@ -0,0 +1,18 @@
+//
+//  CoughLogView.swift
+//  CoughRecoder
+//
+//  Created by 原田佳祐 on 2025/08/05.
+//
+
+import SwiftUI
+
+struct CoughLogView: View {
+    var body: some View {
+        Text("咳記録ログ画面です")
+    }
+}
+
+#Preview {
+    CoughLogView()
+}
