b4f5e41f7bb01cad4dafd4677784423d5ae03885
kei0101xw <kei0101xw@icloud.com>
2025-08-05 14:57:12 +0900

    [add] 設定画面を作成

---
+++ CoughRecoder/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CoughRecoder
+//
+//  Created by 原田佳祐 on 2025/08/05.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("設定画面です")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
