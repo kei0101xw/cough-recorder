7a74fe8f5411fce20d0093561aec26de668df17c
kei0101xw <kei0101xw@icloud.com>
2025-08-05 19:12:47 +0900

    [ui] ホーム画面のボタンのUIを変更

--- CoughRecoder/ContentView.swift
+++ CoughRecoder/ContentView.swift
@@ -11,28 +11,81 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                Spacer()
                 Text("咳記録アプリ")
                     .font(.system(size: 60))
-                    .padding()
-                NavigationLink(destination: InputFormView()) {
-                    Text("スタート")
-                        .padding()
-                        .font(.system(size: 40))
-                        .frame(width: 300)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-                .padding()
+                
+                Spacer()
                 
                 HStack {
-                    NavigationLink(destination: SettingsView()) {
-                        Text("設定")
+                    NavigationLink(destination: InputFormView()) {
+                        VStack {
+                            Spacer()
+                            Text("録音する")
+                                .font(.system(size: 50))
+                                .bold()
+                            Spacer()
+                            Image(systemName: "mic.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 100)
+                            Spacer()
+                        }
+                        .frame(width: 500, height: 400)
+                        .padding()
+                        .background(Color.startButton.opacity(0.1))
+                        .foregroundColor(.startButton)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.startButton, lineWidth: 10)
+                        )
+                        .cornerRadius(10)
                     }
-                    NavigationLink(destination: CoughLogView()) {
-                        Text("咳記録ログ")
+                    .padding()
+                    VStack {
+                        NavigationLink(destination: CoughLogView()) {
+                            VStack {
+                                Text("咳記録ログ")
+                                    .font(.system(size: 50))
+                                    .bold()
+                                Image(systemName: "list.bullet.rectangle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 60)
+                            }
+                            .frame(width: 500, height: 180)
+                            .padding()
+                            .background(Color.logButton.opacity(0.1))
+                            .foregroundColor(.logButton)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.logButton, lineWidth: 10)
+                            )
+                            .cornerRadius(10)
+                        }
+                        NavigationLink(destination: SettingsView()) {
+                            VStack {
+                                Text("設定")
+                                    .font(.system(size: 50))
+                                    .bold()
+                                Image(systemName: "gearshape.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 60)
+                            }
+                            .frame(width: 500, height: 180)
+                            .padding()
+                            .background(Color.settingButton.opacity(0.1))
+                            .foregroundColor(.blue)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 10)
+                            )
+                            .cornerRadius(10)
+                        }
                     }
                 }
+                Spacer()
             }
         }
     }
