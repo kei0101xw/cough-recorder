86136c266a53497751d67eecc186e04ee36ea396
kei0101xw <kei0101xw@icloud.com>
2025-08-05 14:56:54 +0900

    [add] フォーム入力画面を作成

---
+++ CoughRecoder/InputFormView.swift
@@ -0,0 +1,18 @@
+//
+//  InputFormView.swift
+//  CoughRecoder
+//
+//  Created by 原田佳祐 on 2025/08/05.
+//
+
+import SwiftUI
+
+struct InputFormView: View {
+    var body: some View {
+        Text("フォーム入力欄です")
+    }
+}
+
+#Preview {
+    InputFormView()
+}
