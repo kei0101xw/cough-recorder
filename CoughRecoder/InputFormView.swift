4dcdefad57e18d7d170f20be4b71f20e632f610a
kei0101xw <kei0101xw@icloud.com>
2025-08-05 17:16:31 +0900

    [add] フォーム画面の中身を記述

--- CoughRecoder/InputFormView.swift
+++ CoughRecoder/InputFormView.swift
@@ -8,10 +8,69 @@
 import SwiftUI
 
 struct InputFormView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State var name: String = ""
+    @State var hospital: String = ""
+    @State var language: String = ""
+    @State var disease: String = ""
+    
     var body: some View {
-        Text("フォーム入力欄です")
+        VStack {
+            Text("今から記録する人の情報を入力")
+                .font(.system(size: 50))
+                .padding()
+            
+            Form {
+                Section(header: Text("基本情報").font(.system(size: 30))) {
+                    TextField("名前", text: $name)
+                        .padding(.vertical, 10)
+                        .frame(height: 70)
+                        .font(.system(size: 30))
+                    TextField("病院名", text: $hospital)
+                        .padding(.vertical, 10)
+                        .frame(height: 70)
+                        .font(.system(size: 30))
+                    TextField("言語", text: $language)
+                        .padding(.vertical, 10)
+                        .frame(height: 70)
+                        .font(.system(size: 30))
+                    TextField("病気", text: $disease)
+                        .padding(.vertical, 10)
+                        .frame(height: 70)
+                        .font(.system(size: 30))
+                }
+            }
+            
+            HStack {
+                Button(action: {
+                    dismiss() // 前の画面に戻る
+                }) {
+                    Text("ホームへ戻る")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .font(.system(size: 40))
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                }
+                NavigationLink(destination: PreRecordingView()) {
+                    Text("次へ")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .font(.system(size: 40))
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
+
 
 #Preview {
     InputFormView()
