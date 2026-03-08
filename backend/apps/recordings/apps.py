298b63830040afb8e092390d546a9f4a18b3df14
kei0101xw <kei0101xw@icloud.com>
2026-03-08 15:10:55 +0900

    [change] アプリ名を修正

--- backend/apps/recordings/apps.py
+++ backend/apps/recordings/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecordingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recordings'
+    name = 'apps.recordings'
