3e093af64e75c7672e686e98d767ee2625f70f66
kei0101xw <kei0101xw@icloud.com>
2026-03-10 02:22:05 +0900

    [change] Adminのユーザー設定画面の改修

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import CustomUser, Role
+from django.contrib.auth.admin import UserAdmin
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('email', 'name', 'role', 'facility', 'is_staff', 'is_superuser')
+    readonly_fields = ('groups', 'user_permissions', 'date_joined', 'last_login', 'created_at', 'updated_at')
 
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'name', 'password', 'role', 'facility', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'name', 'password1', 'password2', 'role', 'facility', 'is_staff', 'is_superuser'),
+        }),
+    )
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
