e241d302d5145cfddb81bfe844315426cee34b34
kei0101xw <kei0101xw@icloud.com>
2026-03-11 15:39:40 +0900

    [add] patiend_codeから患者データを取得するAPIを追加

--- backend/apps/patients/views.py
+++ backend/apps/patients/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Patient
 from .serializer import PatientSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
@@ -14,6 +17,30 @@ class PatientViewSet(viewsets.ModelViewSet):
             return Patient.objects.all()
 
         return Patient.objects.filter(facility=user.facility)
-    
+
+
     def perform_create(self, serializer):
         serializer.save(facility=self.request.user.facility)
+
+
+    # TODO: adminユーザーがデータを取得する際に複数患者データが取れる問題を修正する
+    @action(detail = False, methods=['get'], url_path='search')
+    def search_by_patient_code(self, request):
+        patient_code = request.query_params.get("patient_code", None)
+
+        if patient_code is None:
+            return Response(
+                {"detail": "patient_code is required."},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        patient = self.get_queryset().filter(patient_code = patient_code).first()
+
+        if patient is None:
+            return Response(
+                {"detail": "Patient not found."},
+                status = status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer: PatientSerializer = self.get_serializer(patient)
+        return Response(serializer.data)
