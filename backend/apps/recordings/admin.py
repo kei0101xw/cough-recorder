4fdd64a79402c5184e768fd6f3f552da3becf34d
kei0101xw <kei0101xw@icloud.com>
2026-03-17 20:40:48 +0900

    [add] 録音管理画面に症状および状態のインライン表示を追加

--- backend/apps/recordings/admin.py
+++ backend/apps/recordings/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Recording
+from .models import Recording, RecordingCondition, RecordingSymptom
+
+class RecordingConditionInline(admin.TabularInline):
+    model = RecordingCondition
+    extra = 0
+
+class RecordingSymptomInline(admin.TabularInline):
+    model = RecordingSymptom
+    extra = 0
 
 @admin.register(Recording)
 class RecordingAdmin(admin.ModelAdmin):
     model = Recording
     list_display = ("id", "user", "patient", "dementia", "status", "recorded_at", "created_at", "updated_at")
+    inlines = [RecordingConditionInline, RecordingSymptomInline]
